e059f48d4f4041539643ad8d1d0be538f6e2499e
johann1sebastian <82532258+johan26sebastian@users.noreply.github.com>
2021-06-01 21:55:51 -0500

    n

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 /* este es un comentario 
 de linea multiple*/
 // las variables se crean con las palabras clave var, let
+// hay tipos de datos: el tipo string me agrupa el tipo cadena de texto.
 console.log ('hola compañeros');
 let variable = 0;
 console.log (variable);
 variable = '20';
 console.log (variable);
+// esta variabre guarda un dato tipo numero
 variable = 20;
 console.log (variable);
 var salario = 200000;
 var sueldo = '200000';
+// hay un tipo de dato llamado Boolean, el cual tiene dos valores: true y false
+var comparacion = sueldo == salario;
