1061d3fce24aa62b881ff672edf44bd33c159e9c
johann1sebastian <82532258+johan26sebastian@users.noreply.github.com>
2021-06-01 21:20:05 -0500

    ye

---
+++ index.js
@@ -0,0 +1,13 @@
+// este es un comentario de una sola linea
+/* este es un comentario 
+de linea multiple*/
+// las variables se crean con las palabras clave var, let
+console.log ('hola compañeros');
+let variable = 0;
+console.log (variable);
+variable = '20';
+console.log (variable);
+variable = 20;
+console.log (variable);
+var salario = 200000;
+var sueldo = '200000';
